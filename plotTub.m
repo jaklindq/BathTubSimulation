a314b303c19a50e769432794bf38d5633e9cd059
jakob <jaklindq@student.chalmers.se>
2016-12-10 20:44:33 +0100

    Created a plot function. For now I set the mesh parameter to h_max since that is already the upper bound of the smoothness of the surface plot.

---
+++ plotTub.m
@@ -0,0 +1,10 @@
+function [] = plotTub(xi,p,hmax)
+x = p(1,:);
+y = p(2,:);
+x_edge=[floor(min(x)):hmax:ceil(max(x))];
+y_edge=[floor(min(y)):hmax:ceil(max(y))];
+[X,Y]=meshgrid(x_edge,y_edge);
+Z=griddata(x,y,xi,X,Y);
+surf(X,Y,Z)
+end
+
