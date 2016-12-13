ae0926d6dc61324b2a9fd46e0dcbb83d392a996c
jakob <jaklindq@student.chalmers.se>
2016-12-13 10:40:33 +0100

    Super simple element area function added

---
+++ ElemArea.m
@@ -0,0 +1,8 @@
+function ar=ElemArea(NodeCoords)
+
+% returns the area of a triangular element with given node coordinates
+
+  x1 = NodeCoords(1,:);
+  x2 = NodeCoords(2,:);
+  ar = (x1(2)-x1(1))*(x2(3)-x2(1))-(x1(3)-x1(1))*(x2(2)-x2(1));
+  ar = ar/2;
